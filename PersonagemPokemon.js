a36b74b053e663ea5a343ba7c234e8a3c8f753e0
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-26 17:37:24 -0300

    Create PersonagemPokemon.js
    Explicação para variáveis em código

---
+++ PersonagemPokemon.js
@@ -0,0 +1,10 @@
+//Pokemon
+let nomepokemon = "Pikachu"
+let nivelPokemon = 20
+let pontosDeVida = 45
+let pokemonSexo = 'M'
+let selecionavel = false
+//Tipo de variáveis
+//number são as variaveis que armazenam números
+//boolean são as variáveis true and false
+//string são variáveos que armazena textos
