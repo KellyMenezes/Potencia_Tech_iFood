c2ebc370c499a6dd759447d79dc5f20c8000b04e
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-26 17:30:19 -0300

    Contantes.js
    Explicações sobre contantes

---
+++ Contantes.js
@@ -0,0 +1,5 @@
+const notificacao = "Pokemon Go diz: "
+//Uma constante é uma variavel que não muda o valor,que é fixo ,é um valor que não pode ser alterado em Java Script...
+//Já a variável pode mudar o valor alocado na memória,essa é a diferença entre as duas.
+console.log(notificacao,"Tem um novo pokemon na região")
+console.log(notificacao,"Você foi derrotado")
