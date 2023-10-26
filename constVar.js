fcb83b27173e637a3dac9cef4fba780db0719cb1
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-26 17:39:15 -0300

    Create constVar.js
    Explicação para variáveis e contantes.

---
+++ constVar.js
@@ -0,0 +1,13 @@
+let poteCafe = "Café ouro"
+let poteAcucar = "Açúcar refinado"
+let poteBiscoitos = "Biscoito de Chocolate"
+const cozinhaVovó = "Na cozinha da vovó tem :"
+
+console.log(cozinhaVovó)
+console.log(poteCafe,"-",poteAcucar,"-",poteBiscoitos)
+
+poteCafe = "Café Três Corações"
+poteAcucar = "Açúcar Mascado"
+//Após declarar uma variável não precisa declarar o valor dela novamente ,Assim   variável = "conteudo que quer alterar"
+console.log(cozinhaVovó)
+console.log(poteCafe,"-",poteAcucar,"-",poteBiscoitos)
