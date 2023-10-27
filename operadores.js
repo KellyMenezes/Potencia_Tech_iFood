61dcf2f815e897b115d118f9a4e04c415e6c3b37
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-27 14:45:43 -0300

    Create operadores.js
    Operadores aritméticos e como utilizá-los

---
+++ operadores.js
@@ -0,0 +1,8 @@
+///Todos os códigos são sequênciais
+let numero = 12 
+//Como utilizar alguns operadores aritmético
+console.log("A soma de 12 + 15 é",numero+15)
+console.log("A subtração de 12 -15 é",numero-15)
+console.log("Divisão de 12 por 3 é",numero/3)
+console.log('A multiplicação de 12 por 5 é',numero*5)
+console.log("O resto de 12 por 5 é",12%5)
