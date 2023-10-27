dd777849a5cc5f90990f786189b6f67a3b528d8e
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-27 14:59:27 -0300

    Create incrementoDecremento.js
    incremento e decremento em código...

---
+++ incrementoDecremento.js
@@ -0,0 +1,9 @@
+let contador = 1
+//incremento adiciona +1 indepentende do número e pode ser aumentado mais quantas vezes você quiser.
+contador++
+contador++
+console.log(contador)
+//decremento subtrai -1 independente do número e pode ser diminuido quantas vezes você quiser
+contador--
+contador--
+console.log(contador)
