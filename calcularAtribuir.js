466db97aa1ca2b7ed5012d4dc34be5d75cfdd073
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-27 16:14:08 -0300

    calcularAtribuir.js
    Como atribuir ou diminuir valores

---
+++ calcularAtribuir.js
@@ -0,0 +1,8 @@
+let deposito = 500
+//para adicionar um valor a uma variável +=
+deposito+= 100
+console.log("Seu deposito tem",deposito,"reais...")
+//para diminuir o valor  a uma variável
+deposito -= 270
+console.log("Seu deposito tem",deposito,"reais...")
+//outros atribuidores de valor /= ,*=,%=
