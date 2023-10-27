c80413eee9fa74f2ad79fc3573d23f2051d7e39d
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-27 16:25:57 -0300

    escopo.js
    Escopo explicação em código

---
+++ escopo.js
@@ -0,0 +1,5 @@
+//Escorpo são os parente eles determinam a ordem de precendentes em operações
+let resultado = 15-34+((45+23)*34-23)+(32*21)
+//ao utilizar parentes os forçamos a reconher aquela parte do código primeiro
+console.log(resultado)
+console.log(15-34+45+23*34-23+32*21)
