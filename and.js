73b1ea236914c9950b83aba07797d0343dbd0285
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-10-30 18:10:18 -0300

    and.js

---
+++ and.js
@@ -0,0 +1,14 @@
+//Condição and === &&
+//A condição and tem que ter duas afirmações verdadeira caso contrario será falso
+let idade = 21 
+let vistoVerificado = true
+let resultado = (idade>= 18)&&(vistoVerificado === true) 
+console.log(resultado)
+console.log("Parabéns você pode viajar!!!")
+
+//and coletado 100 moedas e 1 estrela
+let moedaColetadas = 100
+let item = "estrela"
+resultado = (moedaColetadas === 100) && (item === "estrela")
+console.log(resultado)
+console.log("Parabéns você ganhou o jogo")
