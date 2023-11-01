cf5ebb0f362d356b9e0ccd2241ab9803786b255d
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-11-01 11:31:09 -0300

    Create condiçãoIf.js
    Explicação em código da condição if

---
+++ condiçãoIf.js
@@ -0,0 +1,11 @@
+let ehLigado = true
+if (ehLigado){
+  console.log("Execultou comando")
+  }
+//-------------------------------------------Explicação da consição if
+let ovos = true
+let leite = false
+let supermarket = ovos && leite
+if(supermarket){
+  console.log("Parabéns pela sua compra")
+}
