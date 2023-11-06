970915d908b9c1c845a6c6ebc25b21c7d43dd7e8
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-11-06 14:24:48 -0300

    condiçãoElseIf.js
    Explicação em código das condições if,elseIf,else

---
+++ condiçãoElseIf.js
@@ -0,0 +1,7 @@
+let marchimellow = "Sim, eu te amo!"
+let res = true
+console.log("Do you love me?")
+if (res === true){console.log(marchimellow)} 
+else if(res === false){console.log("Não,eu não te amo")}
+ else{console.log("Você ainda pergunta?")}
+ 
