8d1f7d00ffec8e854f4ccf659e122d721a2486c4
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-11-06 11:53:36 -0300

    condiçãoElse.js

---
+++ condiçãoElse.js
@@ -0,0 +1,7 @@
+let cartaChegou = "Sim"
+if(cartaChegou === "Sim"){console.log("A carta é minha?")}
+  let minhaCarta = false
+  if (minhaCarta === true){
+    console,log('Você pode abrir!!!')
+  }
+  else{console.log("A carta não é minha!")}
