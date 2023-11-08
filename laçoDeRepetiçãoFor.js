f539763813adef1a7401e552f8bba177dbd9d210
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-11-08 10:45:24 -0300

    laçoDeRepetiçãoFor.js
    Explicação em código do laço for

---
+++ laçoDeRepetiçãoFor.js
@@ -0,0 +1,12 @@
+//For <-- laço de repetição ,é quando um bloco de código é execultado enquanto não atender a uma determinada condição.
+//E caso a condição não seja atendida o bloco é execultado indefinidamente.
+//1Exemplo definido o bloco é execultado até 
+for(let casa = 1; casa < 5; casa++){
+    console.log(casa)
+    console.log("Você comprou uma nova casa")
+}
+//2Exemplo indefinidamente execultado o bloco é executado
+for(let casa = 1; casa < 5; casa--){
+    console.log(casa)
+    console.log("Você comprou uma nova casa")
+}
