1ab9c1d30fad31ff0145dca88475d97094eb90b4
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-11-09 11:09:07 -0300

    laçoWhile.js
    Explicação do laço while em código

---
+++ laçoWhilw.js
@@ -0,0 +1,8 @@
+//laço de repetição while é um laço mais facil de compreender visualmente,uma das diferença que notei é que a variavel é criada fora do laço de repetição
+
+marchimellow = 0
+while(marchimellow < 10){
+    marchimellow++
+    console.log("Parabéns você ganhou um novo marchimellows")
+    console.log("Machimellows:",marchimellow)
+}
