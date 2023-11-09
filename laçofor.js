f732baecbc6e1a36f5ca459301d8a39e7a6d47dc
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-11-09 10:49:45 -0300

    laçofor.js
    Exemplo de como utilizar o laço for

---
+++ laçofor.js
@@ -0,0 +1,8 @@
+//Neste laço for criei uma variavel fora do laço para pontos de vida e a cada poção tomada mais um ponto de vida foi adicionado 
+
+let pontosVida = 0
+for(pocaoVida = 1;pocaoVida <= 10;pocaoVida ++){
+    console.log(pontosVida ++)
+    console.log("Parabéns sua vida aumentou!")
+}
+console.log("Pontos de Vida:",pontosVida)
