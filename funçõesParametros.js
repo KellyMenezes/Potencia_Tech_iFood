2f53499441a38273e47644998c4be2aa2c0840b5
Kelly Menezes <106978926+KellyMenezes@users.noreply.github.com>
2023-11-16 14:11:22 -0300

    funçõesParametros.js
    Explicação em código sobre funções e parametros

---
+++ funçõesParametros.js
@@ -0,0 +1,8 @@
+//Funções com parametros
+//É quando criamos uma variavel dentro de uma função ,ou que queremos diferentes resultados para a mesma coisa alterando somente o que queremos mudar dentro da função
+paqueca(" com chocolate")
+paqueca(" com doce de leite")
+paqueca(" com leite moça")
+function paqueca(panqueca){
+    console.log("Panqueca"+panqueca)
+}
